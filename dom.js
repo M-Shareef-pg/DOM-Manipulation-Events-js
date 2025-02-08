ef05351f37df07a46e1a081177fad88a46fe6f63
M-Shareef-pg <najmussaqibpro@gmail.com>
2025-02-08 15:20:00 +0500

    half completed

--- dom.js
+++ dom.js
@@ -12,6 +12,7 @@ console.log(classP);
 let btn = document.getElementsByTagName("button");
 console.dir(btn);
 console.log(btn);
+console.log(btn.tagName);
 
 // Selecting by Query Selector (will pick first element)
 let query = document.querySelector("div");
@@ -22,3 +23,25 @@ console.log(query);
 let allquery = document.querySelectorAll("div");
 console.dir(allquery);
 console.log(allquery);
+
+// properties
+// tagName
+let Sp = document.querySelector("span");
+console.dir(Sp);
+console.log(Sp);
+console.log(Sp.tagName);
+// innerText
+console.log(Sp.innerText);
+
+let collection = document.querySelector("#parent-div")
+let access = document.querySelector("#parent-div").firstChild;
+console.log(access);
+console.dir(collection);    
+console.log(collection.tagName);
+console.log(collection.innerText);
+
+
+
+let cNode = document.querySelector("#commentnode").firstChild;
+console.log(cNode);
+
